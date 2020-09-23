197868760bce88e23c1d59183b5aa2864b3bd75f
JeanPOULET <mathieu.bart3@gmail.com>
2020-09-23 17:03:17 +0200

    first

---
+++ src/test/java/m1graf2020/AppTest.java
@@ -0,0 +1,20 @@
+package m1graf2020;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
